299b92744b0da4efff62cc2f44dca121faa1ad5d
virajj014 <76771864+virajj014@users.noreply.github.com>
2021-01-18 13:29:05 +0530

    Add files via upload

---
+++ arithematicoperator.cpp
@@ -0,0 +1,10 @@
+//operator se hum apne variables ka operation krte h
+#include <iostream>
+using namespace std;
+int main(){
+    int myNum = 9%4;
+    cout <<myNum;
+    return 0;
+}
+
+
---
+++ trigo.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+
+    cout<< cos(1.047)<<"\n";   //	Returns the cosine of x, in radians 60 dgree = 1.0472 rad  
+    cout<< sin(0.524)<<"\n";   //   Returns the cosine of x, in radians 30 degree = 0.523599
+    cout<< tan(0.786)<<"\n";   //   Returns the cosine of x, in radians 45 degree = 0.7853599
+           // Degree * π/180 = Radian
+
+    // ---------------------inverse trigo-------------------------
+
+    cout<< acos(0.5)<<"\n";	//Returns the arccosine of x, in radians   cos(60) = 1/2 = 0.5 --> 60 degree = 1.0472
+           // Degree * π/180 = Radian
+    cout<< asin(0.5)<<"\n"; //Returns the arcsine of x, in radians     sin(30) =  1/2 = 0.5 --> 30 degree = 0.523599 
+    cout<< atan(1)<<"\n";	//Returns the arctangent of x, in radians tan(45)  = 1 -->  45 degree =  0.785398
+
+}
+
+
